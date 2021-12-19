c3dc44af11b3387fe31935562b62b758eafb0d92
רוני כהן <ronico612@gmail.com>
2021-12-19 22:24:56 +0200

    1

--- QuickDeliveryServer/Controllers/QuickDeliveryAPI.cs
+++ QuickDeliveryServer/Controllers/QuickDeliveryAPI.cs
@@ -41,26 +41,6 @@ namespace QuickDeliveryServer.Controllers
                 .ThenInclude(p => p.ProductType).ToList();
         }
 
-        [Route("GetAgeTypes")]
-        [HttpGet]
-        public List<AgeProductType> GetAgeTypes([FromQuery] int shopID)
-        {
-            List<AgeProductType> types = context.AgeProductTypes
-                .Include(apt => apt.Products.Where(p => p.ShopId == shopID))
-              //  .ThenInclude(apt => apt.Product)
-                .ToList();
-            return types;
-        }
 
-        [Route("GetProductTypesForSelectedAge")]
-        [HttpGet]
-        public List<ProductType> GetProductTypesForSelectedAge([FromQuery] int ageTypeProductID, int shopID)
-        {
-            List<ProductType> types = context.ProductTypes
-                .Include(pt => pt.Products.Where(p => p.ShopId == shopID && p.AgeProductTypeId == ageTypeProductID))
-                //  .ThenInclude(apt => apt.Product)
-                .ToList();
-            return types;
-        }
     }       
 }
