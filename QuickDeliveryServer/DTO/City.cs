0a9e0e496d738fd98422d11953cb5224c08336a6
רוני כהן <ronico612@gmail.com>
2022-04-29 12:06:08 +0300

    1

---
+++ QuickDeliveryServer/DTO/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarpoolApp.DTO
+{
+    class City
+    {
+        public string semel_yeshuv { get; set; }
+        public string name { get; set; }
+        public string english_name { get; set; }
+        public string semel_napa { get; set; }
+        public string shem_napa { get; set; }
+        public string semel_lishkat_mana { get; set; }
+        public string lishka { get; set; }
+        public string semel_moatza_ezorit { get; set; }
+        public string shem_moaatza { get; set; }
+    }
+}
